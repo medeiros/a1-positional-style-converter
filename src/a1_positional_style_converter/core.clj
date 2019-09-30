5ba12e71d25909f5dc5fda4596f12ef6aa04921a
Daniel Medeiros <daniel@arneam.com>
2019-09-30 05:40:03 -0300

    Correct algorithm to a1 to positional conversion
    The previous code was broke. It is working now, proved by unit testing
    passing.

--- src/a1_positional_style_converter/core.clj
+++ src/a1_positional_style_converter/core.clj
@@ -9,5 +9,7 @@
 (defn convert-a1-to-positional
   "Convert an A1 core to a positional value starting at 0."
   [c1code]
-  (reduce #(* % (* 26 ?))
-          (map #(- (int (.charAt c1code %)) 65) (range 0 (count c1code)))))
+  (dec(reduce +
+              (map #(int (* %1 (Math/pow 26 %2)))
+                   (into [] (map #(- (int (.charAt % 0)) 64) (clojure.string/split c1code #"")))
+                   (into [] (reverse (range 0 (count c1code))))))))
--- test/a1_positional_style_converter/core_test.clj
+++ test/a1_positional_style_converter/core_test.clj
@@ -4,4 +4,11 @@
 
 (deftest a-test
   (testing "FIXME, I fail."
-    (is (= (convert-a1-to-positional "A") 1))))
+    (is (= (convert-a1-to-positional "A") 0))
+    (is (= (convert-a1-to-positional "AA") 26))
+    (is (= (convert-a1-to-positional "E") 4))
+    (is (= (convert-a1-to-positional "A") 0))
+    (is (= (convert-a1-to-positional "J") 9))
+    (is (= (convert-a1-to-positional "AC") 28))
+    (is (= (convert-a1-to-positional "AB") 27))
+    (is (= (convert-a1-to-positional "BA") 52))))
