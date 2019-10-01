9a0420fa02f41ab3a00b37ae9ddbf7a427b67f9b
Daniel Medeiros <daniel@arneam.com>
2019-10-01 05:10:56 -0300

    Refactor code to be more expressive
    Replace imperative definitions such as (if (= 0 count(args))) to
    (if (empty? args)). This way, the code seems more elegant and clean,
    since is easier to understand.

--- src/a1_positional_style_converter/core.clj
+++ src/a1_positional_style_converter/core.clj
@@ -17,17 +17,11 @@
       (clojure.string/join (map char (map #(+ % 65) (cons x l))))
       (recur (dec (int (/ x 26))) (cons (rem x 26) l)))))
 
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-
-  (if (= 0 (count args))
+(defn -main [& args]
+  (if (empty? args)
     (println "params: [pos|a1] [pos value|a1 value]")
-    (do
-      (let [x (nth args 0)]
-        (case x
-          "pos" (println (convert-positional-to-a1 (Integer/parseInt (nth args 1))))
-          "a1" (println (convert-a1-to-positional (nth args 1))))))))
-
-
+    (let [type (nth args 0)
+          param (nth args 1)]
+      (case type
+        "pos" (println (convert-positional-to-a1 (Integer/parseInt param)))
+        "a1" (println (convert-a1-to-positional param))))))
